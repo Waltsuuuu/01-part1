a4e69c9ed6c58c042432675ce06fd447d0d9fc92
Waltsuuu <waltteriheino@gmail.com>
2024-08-16 13:10:19 +0300

    Komponentin tila ja tapahtumankäsittely-Komponenttien-apufunktiot

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 const Hello = (props) => {
-  console.log(props)
+  const bornYear = () => {
+    const yearNow = new Date().getFullYear()
+    return yearNow - props.age
+  }
   return (
     <div>
       <p>Hello {props.name}, you are {props.age} years old</p>
+      <p>So you were probably born {bornYear()}</p>
     </div>
   )
 }
@@ -14,8 +18,8 @@ const App = () => {
   return (
     <div>
       <h1>Greetings</h1>
-
-      <Hello name="Waltteri" />
+      <Hello name="Waltteri" age={10 + 20}/>
+      <hr></hr>
       <Hello name={nimi} age={ika}/>
     </div>
   )
