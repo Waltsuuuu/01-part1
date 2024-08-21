8b9b627f74e6b0a536f4105f9699467557bb2ad1
Waltsuuu <waltteriheino@gmail.com>
2024-08-21 10:24:27 +0300

    Finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import propTypes from 'prop-types';
 
 
-const Hello = (props) => {
-  const bornYear = () => {
-    const yearNow = new Date().getFullYear()
-    return yearNow - props.age
-  }
+const Hello = ({ name, age }) => {
+    const bornYear = () => new Date().getFullYear() - age
+  
   return (
     <div>
-      <p>Hello {props.name}, you are {props.age} years old</p>
+      <p>Hello {name}, you are {age} years old</p>
       <p>So you were probably born {bornYear()}</p>
     </div>
   )
